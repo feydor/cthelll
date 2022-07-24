009ffcc6694fcc8123e86c6bdc8623668256a2ed
feydor <vreyes85739@outlook.com>
2022-07-23 23:18:04 -0500

    church-encoded reals

--- lambda/lambda.c
+++ lambda/lambda.c
@@ -8,7 +8,23 @@ bool T(void) { return true; }
 bool F(void) { return false; }
 Pred *True(Pred *x, Pred *y) { return x; }
 Pred *False(Pred *x, Pred *y) { return y; }
-Pred *Not(Pred *x) { return x() ? F : T; }
+Pred *Not(Pred *x) {
+    return x() ? F : T;
+}
+Pred *And(Pred *x, Pred *y) {
+    return x() ? y : F; 
+}
+Pred *Or(Pred *x, Pred *y) {
+    return x() ? T : y; 
+}
+Pred *Xor(Pred *x, Pred *y) {
+    return Or(And(x, Not(y)), And(Not(x), y));
+}
+
+typedef int Real(void);
+int Nil(void) { return 0; }
+int S(int x) { return x + 1; }
+int Num(Real *x) { return x(); }
 
 double reduce_itr(double data[], size_t len, double acc, size_t start, void f(double *, double)) {
     if (start == len) return acc;
@@ -31,7 +47,7 @@ void map(void *list, size_t n_elems, size_t size, void f(void *)) {
         f((void *)list+i);
 }
 
-// TODO: varargs, printf like func
+
 void printarr(double arr[], size_t n_elems) {
     printf("[ ");
     for (size_t i = 0; i < n_elems; ++i)
@@ -39,16 +55,39 @@ void printarr(double arr[], size_t n_elems) {
     printf("]\n");
 }
 
+char *P(Pred *x) { return x() ? "T" : "F"; }
 int main(void) {
     double data[] = {5, 10, 15, 20, 25};  
     double res = reduce(data, len(data), 0, sum);
-    printf("result: %f\n", res);
-    
-    printarr(data, len(data));
     map(data, len(data), sizeof(double), inc_dbl);
-    printarr(data, len(data));
     
-    printf("False(T, F) = %s\n", False(T, F) ? "T" : "F");
-    printf("True(T, F) = %s\n", True(T, F) ? "T" : "F");
-    printf("Not(True(T, F)) = %s\n", Not(True(T, F)) ? "T" : "F");
+    printf("=== church-encoded logic primitives ===\n");
+    printf("False(T, F) = %s\n", P(False(T, F)));
+    printf("True(T, F) = %s\n", P(True(T, F)));
+    printf("Not(True(T, F)) = %s\n", P(Not(True(T, F))));
+    printf("And(True(T,F), True(T,F)) = %s\n", P(And(True(T, F), True(T, F))));
+    printf("And(True(T,F), False(T,F)) = %s\n", P(And(True(T, F), False(T, F))));
+    printf("And(False(T,F), True(T,F)) = %s\n", P(And(False(T, F), True(T, F))));
+    printf("And(False(T,F), False(T,F)) = %s\n", P(And(False(T, F), False(T, F))));
+    printf("Or(True(T,F), True(T,F)) = %s\n", P(Or(True(T, F), True(T, F))));
+    printf("Or(True(T,F), False(T,F)) = %s\n", P(Or(True(T, F), False(T, F))));
+    printf("Or(False(T,F), True(T,F)) = %s\n", P(Or(False(T, F), True(T, F))));
+    printf("Or(False(T,F), False(T,F)) = %s\n", P(Or(False(T, F), False(T, F))));
+    printf("Xor(True(T,F), True(T,F)) = %s\n", P(Xor(True(T, F), True(T, F))));
+    printf("Xor(True(T,F), False(T,F)) = %s\n", P(Xor(True(T, F), False(T, F))));
+    printf("Xor(False(T,F), True(T,F)) = %s\n", P(Xor(False(T, F), True(T, F))));
+    printf("Xor(False(T,F), False(T,F)) = %s\n", P(Xor(False(T, F), False(T, F))));
+
+    printf("\n=== church-encoded reals ===\n");
+    printf("Nil() = %d\n", Nil());
+    printf("S(Nil()) = %d\n", S(Nil()));
+    printf("S(S(Nil())) = %d\n", S(S(Nil())));
+    printf("S(S(S(Nil()))) = %d\n", S(S(S(Nil()))));
+    printf("S(S(S(S(Nil())))) = %d\n", S(S(S(S(Nil())))));
+    printf("S(S(S(S(S(Nil()))))) = %d\n", S(S(S(S(S(Nil()))))));
+    printf("S(S(S(S(S(S(Nil())))))) = %d\n", S(S(S(S(S(S(Nil())))))));
+    printf("S(S(S(S(S(S(S(Nil()))))))) = %d\n", S(S(S(S(S(S(S(Nil()))))))));
+    printf("S(S(S(S(S(S(S(S(Nil())))))))) = %d\n", S(S(S(S(S(S(S(S(Nil())))))))));
+    printf("S(S(S(S(S(S(S(S(S(Nil()))))))))) = %d\n", S(S(S(S(S(S(S(S(S(Nil()))))))))));
+    printf("S(S(S(S(S(S(S(S(S(S(Nil())))))))))) = %d\n", S(S(S(S(S(S(S(S(S(S(Nil())))))))))));
 }
